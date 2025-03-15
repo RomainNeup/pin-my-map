afb81fdbbdcbf66bd34c2d11710f2ea35eadb506
RomainNeup <71385790+RomainNeup@users.noreply.github.com>
2025-03-15 17:47:15 +0100

    Add swagger documentation to all routes

--- back/src/place/place.controller.ts
+++ back/src/place/place.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, HttpCode, Param, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 import { Private } from "src/auth/auth.decorator";
 import { PlaceService } from "./place.service";
 import { CreatePlaceRequestDto, PlaceDto } from "./place.dto";
@@ -9,25 +9,31 @@ import { CreatePlaceRequestDto, PlaceDto } from "./place.dto";
 export class PlaceController {
     constructor(
         private readonly placeService: PlaceService
-    ) {}
+    ) { }
 
     @Private()
     @Post()
     @HttpCode(201)
+    @ApiResponse({ status: 201, description: 'Place successfully created.' })
+    @ApiResponse({ status: 400, description: 'Failed to create place.' })
     async createPlace(@Body() placeDto: CreatePlaceRequestDto): Promise<void> {
         await this.placeService.create(placeDto);
         return;
     }
-    
+
     @Private()
     @Get()
     @HttpCode(200)
+    @ApiResponse({ status: 200, type: [PlaceDto], description: 'Returns all available places.' })
     async getPlaces(): Promise<PlaceDto[]> {
         return await this.placeService.findAll();
     }
-    
+
     @Private()
     @Get(':id')
+    @HttpCode(200)
+    @ApiResponse({ status: 200, type: PlaceDto, description: 'Returns a specific place by ID.' })
+    @ApiResponse({ status: 404, description: 'Place with the specified ID not found.' })
     async getPlace(@Param('id') id: string): Promise<PlaceDto> {
         return await this.placeService.findOne(id);
     }
--- back/src/saved/saved.controller.ts
+++ back/src/saved/saved.controller.ts
@@ -15,7 +15,7 @@ export class SavedController {
     @Private()
     @Get()
     @HttpCode(200)
-    @ApiResponse({ status: 200, type: [SavedPlaceDto] })
+    @ApiResponse({ status: 200, type: [SavedPlaceDto], description: 'Return all saved places' })
     async getSavedPlaces(@User("id") userId: string): Promise<SavedPlaceDto[]> {
         return await this.savedPlaceService.findAll(userId);
     }
@@ -23,6 +23,9 @@ export class SavedController {
     @Private()
     @Post(':id')
     @HttpCode(201)
+    @ApiResponse({ status: 201, description: 'Place saved' })
+    @ApiResponse({ status: 400, description: 'Place already saved' })
+    @ApiResponse({ status: 404, description: 'Place not found' })
     async savePlace(@User("id") userId: string, @Param('id') id: string): Promise<void> {
         return await this.savedPlaceService.create(userId, id);
     }
@@ -30,44 +33,65 @@ export class SavedController {
     @Private()
     @Delete(':id')
     @HttpCode(204)
+    @ApiResponse({ status: 204, description: 'Place unsaved' })
+    @ApiResponse({ status: 404, description: 'Saved place not found' })
     async unsavePlace(@User("id") userId: string, @Param('id') id: string): Promise<void> {
         return await this.savedPlaceService.delete(userId, id);
     }
 
     @Private()
     @Get(':id')
-    @ApiResponse({ status: 200, type: SavedPlaceDto })
+    @ApiResponse({ status: 200, type: SavedPlaceDto, description: 'Return saved place by id' })
+    @ApiResponse({ status: 404, description: 'Saved place not found' })
     async getPlace(@User("id") userId: string, @Param('id') id: string): Promise<SavedPlaceDto> {
         return await this.savedPlaceService.findOne(userId, id);
     }
 
     @Private()
     @Put(':id/tag/:tagId')
+    @HttpCode(201)
+    @ApiResponse({ status: 201, description: 'Tag linked' })
+    @ApiResponse({ status: 400, description: 'Tag already linked' })
+    @ApiResponse({ status: 404, description: 'Saved place or tag not found' })
     async addTag(@User("id") userId: string, @Param('id') id: string, @Param('tagId') tagId: string): Promise<void> {
         return await this.savedPlaceService.addTag(userId, id, tagId);
     }
 
     @Private()
     @Delete(':id/tag/:tagId')
+    @HttpCode(204)
+    @ApiResponse({ status: 204, description: 'Tag unlinked' })
+    @ApiResponse({ status: 400, description: 'Tag not linked' })
+    @ApiResponse({ status: 404, description: 'Saved place not found' })
     async removeTag(@User("id") userId: string, @Param('id') id: string, @Param('tagId') tagId: string): Promise<void> {
         return await this.savedPlaceService.removeTag(userId, id, tagId);
     }
 
     @Private()
     @Put(':id/comment')
-    async addComment(@User("id") userId: string, @Param('id') id: string, @Body() body: CommentSavedPlaceDto): Promise<void> {
+    @HttpCode(200)
+    @ApiResponse({ status: 200, type: SavedPlaceDto, description: 'Comment added' })
+    @ApiResponse({ status: 400, description: 'Comment must be provided' })
+    @ApiResponse({ status: 404, description: 'Saved place not found' })
+    async addComment(@User("id") userId: string, @Param('id') id: string, @Body() body: CommentSavedPlaceDto): Promise<SavedPlaceDto> {
         return await this.savedPlaceService.addComment(userId, id, body.comment);
     }
 
     @Private()
     @Put(':id/rating')
-    async addRating(@User("id") userId: string, @Param('id') id: string, @Body() body: RatingSavedPlaceDto): Promise<void> {
+    @HttpCode(200)
+    @ApiResponse({ status: 200, type: SavedPlaceDto, description: 'Rating added' })
+    @ApiResponse({ status: 400, description: 'Rating must be between 0 and 5' })
+    @ApiResponse({ status: 404, description: 'Saved place not found' })
+    async addRating(@User("id") userId: string, @Param('id') id: string, @Body() body: RatingSavedPlaceDto): Promise<SavedPlaceDto> {
         return await this.savedPlaceService.addRating(userId, id, body.rating);
     }
 
     @Private()
     @Patch(':id/done')
     @HttpCode(204)
+    @ApiResponse({ status: 204, description: 'Done toogled' })
+    @ApiResponse({ status: 404, description: 'Saved place not found' })
     async toogleDone(@User("id") userId: string, @Param('id') id: string): Promise<void> {
         return await this.savedPlaceService.toogleDone(userId, id);
     }
