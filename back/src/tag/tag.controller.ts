dd8052439a902355cc7c731ea5bba41926709dc4
RomainNeup <71385790+RomainNeup@users.noreply.github.com>
2025-03-15 17:47:15 +0100

    Add swagger documentation to all routes

--- back/src/tag/tag.controller.ts
+++ back/src/tag/tag.controller.ts
@@ -1,23 +1,30 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Post } from "@nestjs/common";
 import { TagService } from "./tag.service";
 import { Private } from "src/auth/auth.decorator";
 import { CreateTagRequestDto, TagDto } from "./tag.dto";
 import { User } from "src/user/user.decorator";
+import { ApiResponse, ApiTags } from "@nestjs/swagger";
 
 @Controller('tag')
+@ApiTags('Tags')
 export class TagController {
     constructor(
         private readonly tagService: TagService
-    ) {}
+    ) { }
 
     @Private()
     @Post()
+    @HttpCode(201)
+    @ApiResponse({ status: 201, description: 'The tag has been successfully created.' })
+    @ApiResponse({ status: 400, description: 'Bad request. Invalid input data.' })
     async createTag(@Body() tagDto: CreateTagRequestDto, @User("id") userId: string): Promise<void> {
         return await this.tagService.create(userId, tagDto);
     }
 
     @Private()
     @Get()
+    @HttpCode(200)
+    @ApiResponse({ status: 200, type: [TagDto], description: 'Returns all tags for the authenticated user.' })
     async getTags(@User("id") userId: string): Promise<TagDto[]> {
         return await this.tagService.findAll(userId);
     }
